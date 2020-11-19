ae98f5785d7b78b8f461f959808a185b263666ed
dtran003 <dtran003@plattsburgh.edu>
2020-11-19 16:58:17 -0500

    Updates

---
+++ precache-manifest.aeba899fdaf233a5f97f27f0d257d7f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e88fb23a38f7d0743a026e343644b0b5",
+    "url": "/new_app/index.html"
+  },
+  {
+    "revision": "d780b15111c691a475dd",
+    "url": "/new_app/static/css/main.490ec20c.chunk.css"
+  },
+  {
+    "revision": "e5b766b94846820a8a73",
+    "url": "/new_app/static/js/2.f17d942d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/new_app/static/js/2.f17d942d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d780b15111c691a475dd",
+    "url": "/new_app/static/js/main.a388c63d.chunk.js"
+  },
+  {
+    "revision": "ac5d40211a1b49adc63b",
+    "url": "/new_app/static/js/runtime-main.1b97e793.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
